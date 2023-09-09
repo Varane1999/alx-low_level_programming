37b5ab5e7ac41c1a8b1ddf35233c2ab2f976a1b4
Varane1999 <harrowadewale@gmail.com>
2023-09-09 05:38:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int w;
+
+	w = 0;
+	while
+		(w < 10) {
+			putchar(w + '0');
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
