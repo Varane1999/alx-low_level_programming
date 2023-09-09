862dc6132cbb6c98d2f4d0640683b44b37414859
Varane1999 <harrowadewale@gmail.com>
2023-09-09 05:52:02 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char w;
+
+	int d;
+
+	w = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(w <= 'f') {
+			putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
