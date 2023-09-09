de7530504d7238607f1f3cf26ef36baea58d47f7
Varane1999 <harrowadewale@gmail.com>
2023-09-09 05:44:40 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char w;
+
+	w = 'z';
+	while
+		(w >= 'a') {
+			putchar(w);
+			w--;
+		}
+	putchar('\n');
+	return (0);
+}
