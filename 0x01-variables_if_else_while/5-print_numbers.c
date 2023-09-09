e7c1960904509ae23ff79ebfb30a6168b62455b9
Varane1999 <harrowadewale@gmail.com>
2023-09-09 05:19:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int w;
+
+	for (w = 0; w < 10; w++)
+		printf("%d", w);
+	putchar('\n');
+	return (0);
+}
