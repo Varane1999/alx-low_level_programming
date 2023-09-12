70fdad7c25f7818f88695f2118073a510aadee31
Varane1999 <harrowadewale@gmail.com>
2023-09-12 10:27:12 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-	char w;
 	int i;
+	char w;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (w = 'a'; w <= 'z'; w++)
+		for (w = 'a' ; w <= 'z' ; w++)
 			_putchar(w);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
