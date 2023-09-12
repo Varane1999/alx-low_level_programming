5d8a041e100395e028dba09d2fd0c3b1adc105e8
Varane1999 <harrowadewale@gmail.com>
2023-09-12 12:56:18 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the abolute value of an integer.
+ * @c: The parameter to be checked.
+ * Return: Absolute value of number or zero
+ */
+
+int _abs(int w)
+
+{
+	if (w < 0)
+	{
+	int abs_val;
+
+	abs_val = w * -1;
+	return (abs_val);
+	}
+	return (w);
+}
