dbc5d4ae7ede608e197f8ce867dc0d37d1efd84c
Varane1999 <harrowadewale@gmail.com>
2023-09-12 14:03:30 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,19 +3,17 @@
 /**
  * print_last_digit - Prints the last digit of a number.
  * @w: Parameter to be checked.
- * Return: Value of the last digit of number.
+ * Return: k
  */
 
 int print_last_digit(int w)
 
 {
-	int last;
+	int k;
 
-	last = w % 10;
-	if (last < 10)
-	{
-	last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+	k = w % 10;
+	if (w < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
 }
