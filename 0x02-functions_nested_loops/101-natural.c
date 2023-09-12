1a7feedb69e01e1ccba4ba6506b68309c5d83c8d
Varane1999 <harrowadewale@gmail.com>
2023-09-12 16:24:28 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Main - in values below 1024, functions to add multiples of three or five
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int sum = 0;
+	int w;
+
+	for (w = 0; w < 1024; w++)
+	{
+		if (w % 3 == 0 || w % 5 == 0)
+			sum = sum + w;
+	}
+
+	printf("%d\n", sum);
+	return (0)
+}
