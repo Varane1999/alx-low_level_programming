8e7594640a135c8eb84c82eae39ae89865bfb8a7
Varane1999 <harrowadewale@gmail.com>
2023-09-12 15:07:14 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two values.
+ * @w1: The first parameter to be computed.
+ * @w2: The second parameter to be computed.
+ * Return: Integer value.
+ */
+
+int add(int w1, int w2)
+
+{
+
+	return (w1 + w2);
+
+}
+
