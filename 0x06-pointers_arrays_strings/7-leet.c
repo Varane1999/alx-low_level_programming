4833e90254dbb03c374923949186044e3a4997e1
Varane1999 <harrowadewale@gmail.com>
2023-09-21 05:52:38 -0700

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 /**
-  *leet - origin function
-  * @n: Function parameter
-  *Return: value of n
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-
 char *leet(char *n)
 {
-	int w, i;
+	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (w = 0; n[w] != '\0'; w++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-	for (i = 0; i < 10; i++)
-	{
-	if (n[w] == s1[i])
-	{
-	n[i] = s2[i];
-	}
-	}
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
 	}
 	return (n);
 }
