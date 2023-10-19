37548763dd9b3f19ab98f59425db15f61546673b
Varane1999 <harrowadewale@gmail.com>
2023-10-19 16:04:08 -0700

    2-add_nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,19 +30,18 @@ int _strlen(const char *s)
 */
 list_t *add_node(list_t **head, const char *str)
 {
-   list_t *add;
+	list_t *add;
 
 
-   add = malloc(sizeof(list_t));
-   if (add == NULL)
-       return (NULL);
-   add->str = strdup(str);
+	add = malloc(sizeof(list_t));
+	if (add == NULL)
+	return (NULL);
+	add->str = strdup(str);
 
 
-   add->len = _strlen(str);
-   add->next = *head;
-   *head = add;
+	add->len = _strlen(str);
+	add->next = *head;
+	*head = add;
 
-
-   return (add);
+	return (add);
 }
