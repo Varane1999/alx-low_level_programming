425a886aa84c952bf6538b4b98a5156881a88c02
Varane1999 <harrowadewale@gmail.com>
2023-10-19 16:09:57 -0700

    2-add_nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,12 +3,10 @@
 #include <string.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
- *
- * Return: the address of the new element, or NULL if it fails
- */
+* _strlen - function that gives the string's length as a result.
+* @s : s is a character
+* Return: value is w
+*/
 
 int _strlen(const char *s)
 {
