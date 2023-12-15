b8c3184d94d3929a508953a0b4e7707e0b8ac8d4
Varane1999 <harrowadewale@gmail.com>
2023-12-15 14:02:13 -0800

    booyaa

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Varane1999/alx-low_level_programming/master/0x18-dynamic_libraries/libbooya.so
-export LD_PRELOAD="$PWD/../libbooya.so"
+wget -P .. https://raw.githubusercontent.com/Varane1999/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
